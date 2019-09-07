d1670f22aa78ca4b8cfe5ecb332335004f086950
Jose B Gomes <jbonigomes@yahoo.com.br>
2019-09-07 22:44:53 +0100

    1.5: course information step5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -6,27 +6,29 @@ import Header from './Header'
 import Content from './Content'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      exercises: 10,
-      name: 'Fundamentals of React',
-    },
-    {
-      exercises: 7,
-      name: 'Using props to pass data',
-    },
-    {
-      exercises: 14,
-      name: 'State of a component',
-    },
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        exercises: 10,
+        name: 'Fundamentals of React',
+      },
+      {
+        exercises: 7,
+        name: 'Using props to pass data',
+      },
+      {
+        exercises: 14,
+        name: 'State of a component',
+      },
+    ],
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
