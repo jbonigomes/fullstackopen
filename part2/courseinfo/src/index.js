3e287bb540ad316c0f51449975b1101411e6ddf7
Jose B Gomes <jbonigomes@yahoo.com.br>
2019-09-10 19:43:37 +0100

    2.5: separate module

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -50,11 +50,11 @@ const App = () => {
   ]
 
   return (
-    <div>
+    <>
       {courses.map((course) => (
         <Course key={course.id} course={course} />
       ))}
-    </div>
+    </>
   )
 }
 
